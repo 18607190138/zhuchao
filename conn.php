1616c6697db6e47d25a6ebfb2b49c6e4d816131d
18607190138 <18607190138@163.com>
2017-07-03 15:58:40 +0800

    1
    1111111111

---
+++ conn.php
@@ -0,0 +1,14 @@
+<?php
+return $config = array(
+//    "host" => "localhost", //数据库连接地址
+//    "user" => "root", //登录用户名
+//    "pass" => "root", //密码
+//    "dbname" => "blog"//数据库
+    "host" => "183.90.188.10", //数据库连接地址
+    "user" => "a0616131335", //登录用户名
+    "pass" => "a65c104a", //密码
+    "dbname" => "a0616131335"//数据库
+);
+
+
+
